a65343338c8416fa48c046e3a3af5d02fa6b87e2
01fe22bca146 <01fe22bca146@kletech.ac.in>
2024-01-30 11:02:18 +0530

    code for biggest number

---
+++ biggest.java
@@ -0,0 +1,18 @@
+public class LargestNo{
+    public static void main(String[] args) {
+
+        int num1 = 10,
+            num2 = 12,
+            num3 = 4;
+
+        if( num1 >= num2 && num1 >= num3)
+            System.out.println(num1+ "is the largest number.");
+
+        else if (num2 >= num1 && num2 >= num3)
+            System.out.println(num2+ " is the largest number.");
+
+        else
+            System.out.println(num3+ "is the largest number.");
+        }
+}
+
